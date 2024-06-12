504baebbdd9edf90def26b8809509b5167afaf17
danya2271 <dany2271@yandex.ru>
2024-06-13 02:52:36 +0500

    Revert "libs:dumputils: remove mediametrics because it doesn't work anymore"
    This reverts commit 0778ff04888109fa1ecb5accda4f9fabb8a90cdf.

--- libs/dumputils/dump_utils.cpp
+++ libs/dumputils/dump_utils.cpp
@@ -32,6 +32,7 @@ static const char* native_processes_to_dump[] = {
         "/system/bin/drmserver",
         "/system/bin/mediadrmserver",
         "/system/bin/mediaextractor", // media.extractor
+        "/system/bin/mediametrics", // media.metrics
         "/system/bin/mediaserver",
         "/system/bin/mediatranscoding", // media.transcoding
         "/system/bin/netd",
